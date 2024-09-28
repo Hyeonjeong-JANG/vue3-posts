7249c7e1678da907105a142ca4713e4fff1aa705
HyeonjeongJang <hyeonjeong.jang.0302@gmail.com>
2024-09-28 21:58:22 +0900

    프로젝트 설정, CRUD, 페이지네이션, 필터

---
+++ src/components/posts/PostItem.vue
@@ -0,0 +1,28 @@
+<template>
+  <AppCard>
+    <h5 class="card-title">{{ title }}</h5>
+    <p class="card-text">
+      {{ content }}
+    </p>
+    <p class="text-muted">{{ createdAt }}</p>
+  </AppCard>
+</template>
+
+<script setup>
+import AppCard from '@/components/AppCard.vue';
+defineProps({
+  title: {
+    type: String,
+    Required: true,
+  },
+  content: {
+    type: String,
+    Required: true,
+  },
+  createdAt: {
+    type: [String, Date, Number],
+  },
+});
+</script>
+
+<style lang="scss" scoped></style>
